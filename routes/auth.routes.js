548202b9b4ca1600e5dec08e2aef97a45330004c
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-24 14:46:19 +0200

    Add: authenticate user -> login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,68 +1,83 @@
 const express = require( 'express' );
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const User = require('../models/User.model');
+const bcrypt = require( 'bcryptjs' );
+const User = require( '../models/User.model' );
 
-// Signup
-router.get('/signup', (req, res, next) => {
-  res.render('auth/signup')
-})
+// NOTE: Signup
+router.get( '/signup', ( req, res, next ) => {
+	res.render( 'auth/signup' );
+} );
 
-router.post('/signup', (req, res, next) => {
-  const {username, email, password} = req.body
+router.post( '/signup', ( req, res, next ) => {
+	const { username, email, password } = req.body;
 
-  //validate password security
-  if(password.length <= 8){
-    res.render('auth/signup', { message: 'Password must have at least 8 characters'})
-    return
-  }
-  if (!/[A-Z]/.test(password) && !/[a-z]/.test(password)) {
-    res.render('auth/signup', { message: 'Password must contain both uppercase and lowercase letters' });
-    return;
-  }
-  if (!/\d/.test(password)) {
-    res.render('auth/signup', { message: 'Password must contain at least one digit' });
-    return;
-  }
-  if (!/[@!#$%^&*()_+=[\]{}|\\,.?:-]/.test(password)) {
-    res.render('auth/signup', { message: 'Password must contain at least one special character' });
-    return;
-  }
+	// NOTE: Validation: validate password security
+	if ( password.length <= 8 ) {
+		res.render( 'auth/signup', { message: 'Password must have at least 8 characters' } );
+		return;
+	}
+	if ( !/[A-Z]/.test( password ) && !/[a-z]/.test( password ) ) {
+		res.render( 'auth/signup', { message: 'Password must contain both uppercase and lowercase letters' } );
+		return;
+	}
+	if ( !/\d/.test( password ) ) {
+		res.render( 'auth/signup', { message: 'Password must contain at least one digit' } );
+		return;
+	}
+	if ( !/[@!#$%^&*()_+=[\]{}|\\,.?:-]/.test( password ) ) {
+		res.render( 'auth/signup', { message: 'Password must contain at least one special character' } );
+		return;
+	}
 
-  User.findOne({username})
-    .then(userFromDb => {
-      if(userFromDb !== null){
-        res.render('auth/signup', { message: 'Username is already taken!'})
-      }
-      else{
-        User.findOne({email})
-          .then(emailUserFromDb => {
-            if(emailUserFromDb !== null){
-              res.render('auth/signup', { message: 'Email is already taken!'})
-            }
-            else{
-              const salt = bcrypt.genSaltSync()
-              const hash = bcrypt.hashSync(password, salt)
-      
-              User.create({username: username, email: email, password: hash})
-                .then(() => {
-                  res.redirect('/')
-                })
-                .catch(err => next(err))
-            }
-          })
-      }
-    })
-})
+	// NOTE: Create User
+	User.findOne( { username } )
+		.then( ( userFromDb ) => {
+			if ( userFromDb !== null ) {
+				res.render( 'auth/signup', { message: 'Username is already taken!' } );
+			} else {
+				User.findOne( { email } )
+					.then( ( emailUserFromDb ) => {
+						if ( emailUserFromDb !== null ) {
+							res.render( 'auth/signup', { message: 'Email is already taken!' } );
+						} else {
+							const salt = bcrypt.genSaltSync();
+							const hash = bcrypt.hashSync( password, salt );
 
-// Login
+							User.create( { username: username, email: email, password: hash } )
+								.then( () => {
+									res.redirect( '/' );
+								} )
+								.catch( ( err ) => next( err ) );
+						}
+					} );
+			}
+		} );
+} );
+
+// NOTE: Login
 router.get( '/login', ( req, res, next ) => {
 	res.render( 'auth/login' );
 } );
 
 router.post( '/login', ( req, res, next ) => {
 	const { username, email, password } = req.body;
-	console.log( `Username: ${username}, Email: ${email}, Password: ${password}` );
+
+	User.findOne( { username } )
+		.then( ( userFromDb ) => {
+			// if user is in database
+			if ( userFromDb ) {
+				// compare password
+				if ( bcrypt.compareSync( password, userFromDb.password ) ) {
+					// create session
+					const sessionUser = {
+						id: userFromDb._id,
+						username: userFromDb.username,
+						role: userFromDb.role,
+					};
+					req.session.sessionUser = sessionUser;
+				}
+			}
+		} );
 } );
 
 module.exports = router;
