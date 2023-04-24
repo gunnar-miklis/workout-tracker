49185a81d578184ccf8a3ca8f6aed0af3a7bd30c
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-24 12:48:43 +0200

    Update: Login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,23 @@
 const express = require( 'express' );
 const router = express.Router();
 
-router.get('/signup', (req, res, next) => {
-  res.render('auth/signup')
-})
+router.get( '/signup', ( req, res, next ) => {
+	res.render( 'auth/signup' );
+} );
 
-router.post('/signup', (req, res, next) => {
-  const {username, email, password} = req.body
-  console.log(`Username: ${username} Password: ${password}`)
-})
+router.post( '/signup', ( req, res, next ) => {
+	const { username, email, password } = req.body;
+	console.log( `Username: ${username} Password: ${password}` );
+} );
+
+router.get( '/login', ( req, res, next ) => {
+	res.render( 'auth/login' );
+} );
+
+router.post( '/login', ( req, res, next ) => {
+	const { username, email, password } = req.body;
+	console.log( `Username: ${username}, Email: ${email}, Password: ${password}` );
+} );
+
+
 module.exports = router;
