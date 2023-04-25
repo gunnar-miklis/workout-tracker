40cfca86fde0410ed408bbea96c974bee07dc43a
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-25 21:19:49 +0200

    Add: comments

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -1,7 +1,10 @@
 const isLoggedIn = ( req, res, next ) => {
+	// if there's a sessionUser...
 	if ( req.session.sessionUser ) {
+		// ...then continue with the middleware flow (or next route)...
 		next();
 	} else {
+		// ...otherwise, redirect to login page
 		res.redirect( '/login' );
 	}
 };
