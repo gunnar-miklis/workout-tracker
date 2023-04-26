4f88c08b64b040db452d78175b368639ca915003
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-04-26 11:39:10 +0200

    bug fixes

--- app.js
+++ app.js
@@ -48,11 +48,14 @@ const indexRoutes = require( './routes/index.routes' );
 const authRoutes = require( './routes/auth.routes' );
 const homeRoutes = require( './routes/home.routes' );
 const trackerRoutes = require( './routes/tracker.routes' );
+const statisticsRoutes = require('./routes/statistics.routes')
+
 // const statisticsRoutes = require( './routes/statistics.routes' );
 app.use( '/', indexRoutes );
 app.use( '/', authRoutes );
 app.use( '/', homeRoutes );
 app.use( '/', trackerRoutes );
+app.use( '/', statisticsRoutes)
 // app.use( '/', statisticsRoutes );
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
