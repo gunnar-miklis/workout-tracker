aaee6582bad978bf138b104493b519ad743f71d9
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-26 20:49:15 +0200

    Implement: render "previous exercises" | Add: "abort" route | Fix: deletion of session properties on abort or save

--- routes/tracker.routes.js
+++ routes/tracker.routes.js
@@ -1,52 +1,80 @@
 const express = require( 'express' );
 const router = express.Router();
 const Workout = require( '../models/Workout.model' );
-const Exercise = require('../models/Exercise.model')
+const Exercise = require( '../models/Exercise.model' );
 const isLoggedIn = require( '../utils/isLoggedIn' );
-const User = require('../models/User.model');
-const session = require('express-session');
-const exercisesList = require('../db/list-of-exercises').exercisesList
+const User = require( '../models/User.model' );
+const exercisesList = require( '../db/list-of-exercises' ).exercisesList;
+const c = require( 'colors' );
+c.setTheme( { log: 'bgBrightYellow' } );
 
 router.get( '/tracker', isLoggedIn, ( req, res, next ) => {
-  res.render('tracker')
-})
+	console.log( c.log( 'req.session.previousWorkoutExercises :>> ' ), req.session.previousWorkoutExercises );
+	if ( req.session.previousWorkoutExercises ) {
+		// get all exercises for this workout session ( contains only ids )
+		const previousWorkoutExercisesIds = req.session.previousWorkoutExercises;
 
-router.post('/tracker', (req, res, next) => {
-  const {title, rep, weight, time, distance} = req.body
+		// populate exercises, wait for all to be populated
+		Promise.all( previousWorkoutExercisesIds.map( ( exerciseId ) => {
+			// populate the ids, to show title, etc. later
+			return Exercise.findById( exerciseId )
+				.populate()
+				.then( ( populated ) => {
+					return populated;
+				} );
+		} ) )
+			.then( ( result ) => {
+				// create an object for all these exercercises
+				const previousWorkoutExercises = {};
+				// need to be stored in a property "exercises", in order to use the partial correctly later
+				previousWorkoutExercises.exercises = result;
+				console.log( c.log( 'result :>> ' ), result );
+				res.render( 'tracker', { previousWorkoutExercises } );
+			} )
+			.catch( ( err ) => next( err ) );
+	} else {
+		res.render( 'tracker' );
+	}
+} );
 
-  if(!exercisesList.some((exercise) => exercise.name === title)){
-    res.render('tracker', { message: 'Exercise not found'})
-    return
-  }
+router.post( '/tracker', ( req, res, next ) => {
+	const { title, rep, weight, time, distance } = req.body;
 
-  Exercise.create({title, rep, weight, time, distance})
-    .then((exercise) => {
-      req.session.workoutExercises = Array.isArray(req.session.workoutExercises) ? req.session.workoutExercises : []
-      req.session.workoutExercises.push(exercise._id)
-      res.redirect('/tracker')
-    })
-    .catch(err => {
-      next(err)
-    })
-})
+	if ( !exercisesList.some( ( exercise ) => exercise.name === title ) ) {
+		res.render( 'tracker', { message: 'Exercise not found' } );
+		return;
+	}
+
+	Exercise.create( { title, rep, weight, time, distance } )
+		.then( ( exercise ) => {
+			req.session.previousWorkoutExercises = Array.isArray( req.session.previousWorkoutExercises ) ? req.session.previousWorkoutExercises : [];
+			req.session.previousWorkoutExercises.push( exercise._id );
+			res.redirect( '/tracker' );
+		} )
+		.catch( ( err ) => next( err ) );
+} );
 
 
-router.post('/saveworkout', (req, res, next) => {
-  const {title, duration} = req.body
-  const exercises = req.session.workoutExercises || [] // initialize the array if it's undefined
-  Workout.create({title, duration, date: new Date(), exercises}) 
-    .then(createdWorkout => {
-      const userId = req.session.sessionUser._id
-      User.findByIdAndUpdate(userId, {$push: {workouts: createdWorkout._id}}).exec()
-        .then((updatedUser) => {
-          console.log('Updated User:', updatedUser)
-          req.session.workoutExercises = []
-          res.redirect('/home')
-        })
-    })
-    .catch(err => {
-      next(err)
-    })
-})
+router.post( '/saveworkout', ( req, res, next ) => {
+	const { title, description, duration } = req.body;
+	const exercises = req.session.previousWorkoutExercises || []; // initialize the array if it's undefined
+	Workout.create( { title, description, duration, date: new Date(), exercises } )
+		.then( ( createdWorkout ) => {
+			const userId = req.session.sessionUser._id;
+			User.findByIdAndUpdate( userId, { $push: { workouts: createdWorkout._id } } ).exec()
+				.then( ( updatedUser ) => {
+					delete req.session.previousWorkoutExercises;
+					res.redirect( '/home' );
+				} );
+		} )
+		.catch( ( err ) => {
+			next( err );
+		} );
+} );
+
+router.post( '/abort', ( req, res, next ) => {
+	delete req.session.previousWorkoutExercises;
+	res.redirect( '/home' );
+} );
 
 module.exports = router;
