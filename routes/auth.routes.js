d048d379544505129dd2237512e380be00feaf9e
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-27 21:58:01 +0200

    Implement: create workout templates on user sign up

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const bcrypt = require( 'bcryptjs' );
 const User = require( '../models/User.model' );
 const isLoggedIn = require( '../utils/isLoggedIn' );
+const Exercise = require( '../models/Exercise.model' );
+const Workout = require( '../models/Workout.model' );
 
 // NOTE: create a session for the user
 const createUserSession = ( req, user ) => {
@@ -12,6 +14,59 @@ const createUserSession = ( req, user ) => {
 		role: user.role,
 	};
 	req.session.sessionUser = sessionUser;
+};
+
+// NOTE: create user templates
+const createUserTemplates = ( res, next, userId ) => {
+	// first get all exercises
+	// COMMENT: ...however we will only use the first 9 exercises (might be a thing to improve later: to find only index [0-8] )
+	Exercise.find()
+		.then( ( exercises ) => {
+			// create some default workouts for the user
+			return workoutTemplates = [
+				{
+					'title': 'Strength Day',
+					'description': 'pushing max power.',
+					'date': '2023-04-21',
+					'duration': '00:32:12',
+					'exercises': [exercises[3]._id, exercises[4]._id, exercises[5]._id],
+				},
+				{
+					'title': 'Endurance Day',
+					'description': 'run to the park, along the river',
+					'date': '2023-04-24',
+					'duration': '01:15:12',
+					'exercises': [exercises[0]._id],
+				},
+				{
+					'title': 'Lifting Day',
+					'description': 'doing some weight exercises in the gym',
+					'date': '2023-04-24',
+					'duration': '00:21:44',
+					'exercises': [exercises[6]._id, exercises[7]._id, exercises[8]._id, exercises[6]._id],
+				},
+			];
+		} )
+		.then( ( workoutTemplates ) => {
+			// create the workouts
+			Workout.create( workoutTemplates )
+				.then( ( createdWorkouts ) => {
+					// get only the ids
+					const updateWorkoutIds = [];
+					updateWorkoutIds.push( createdWorkouts[0]._id );
+					updateWorkoutIds.push( createdWorkouts[1]._id );
+					updateWorkoutIds.push( createdWorkouts[2]._id );
+					// update the user and push the ids from the created workouts to the users workouts array
+					User.findByIdAndUpdate( userId, { $push: { workouts: updateWorkoutIds } }, { new: true } )
+						.then( () => {
+							// redirect to welcome page
+							res.redirect( '/welcome' );
+						} )
+						.catch( ( err ) => next( err ) );
+				} )
+				.catch( ( err ) => next( err ) );
+		} )
+		.catch( ( err ) => next( err ) );
 };
 
 // NOTE: Signup Page
@@ -56,16 +111,22 @@ router.post( '/signup', ( req, res, next ) => {
 
 							User.create( { username: username, email: email, password: hash } )
 								.then( ( newUser ) => {
-									// create session
+									// call function to create session
 									createUserSession( req, newUser );
-									// redirect to welcome page
-									res.redirect( '/welcome' );
+									return newUser._id;
+								} )
+								.then( ( userId ) => {
+									// call function to create user template
+									createUserTemplates( res, next, userId );
+									// COMMENT: redirect happens inside the function (might be a thing to improve later: to call the redirect inside the route)
 								} )
 								.catch( ( err ) => next( err ) );
 						}
-					} );
+					} )
+					.catch( ( err ) => next( err ) );
 			}
-		} );
+		} )
+		.catch( ( err ) => next( err ) );
 } );
 
 // NOTE: Welcome Page
